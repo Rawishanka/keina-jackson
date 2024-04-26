73adc4aa24228306023d09901bf7591f9e22a17a
rasinduRawishanka <Rasindu.rawishanka@gmail.com>
2024-04-27 02:07:52 +0530

    add audio to the home

--- app/page.tsx
+++ app/page.tsx
@@ -33,6 +33,17 @@ const HomePage = () => {
   //   }
   // }, [audioStarted]);
 
+  const [audio] = useState(getAudioInstance);
+
+  useEffect(() => {
+    audio.loop = true; // Loop the audio
+    audio.play(); // Start playing the audio
+
+    return () => {
+      audio.pause(); // Pause the audio when the component unmounts
+    };
+  }, [audio]);
+
 
   return (
     <div>
